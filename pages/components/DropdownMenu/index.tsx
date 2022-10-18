15c24b8805e12b742cc3c64d42b56512452d76ad
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-17 21:27:17 -0300

    feat: add navbar

--- pages/components/DropdownMenu/index.tsx
+++ pages/components/DropdownMenu/index.tsx
@@ -1,22 +1,20 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import {
     HamburgerMenuIcon,
-    DotFilledIcon,
 } from '@radix-ui/react-icons';
 
 export default () => (
   <DropdownMenu.Root>
-    <DropdownMenuTrigger asChild>
-        <IconButton aria-label="Customise options" style={{backgroundColor: scrolled > 30 ? 'black' : 'transparent'}}>
-            <HamburgerMenuIcon />
-        </IconButton>
-        </DropdownMenuTrigger>
+    <DropdownMenu.DropdownMenuTrigger asChild>
+        <HamburgerMenuIcon />
+    </DropdownMenu.DropdownMenuTrigger>
 
-    <DropdownMenu.Content>
-      <DropdownMenu.Label>Label</DropdownMenu.Label>
-      <DropdownMenu.Item>…</DropdownMenu.Item>
-      <DropdownMenu.Item>…</DropdownMenu.Item>
-      <DropdownMenu.Item>…</DropdownMenu.Item>
-    </DropdownMenu.Content>
+    <DropdownMenu.DropdownMenuContent>
+        <DropdownMenu.DropdownMenuRadioGroup>
+        <DropdownMenu.DropdownMenuRadioItem value ='home'>Inicio</DropdownMenu.DropdownMenuRadioItem>
+        <DropdownMenu.DropdownMenuRadioItem value ='encrypt'>Criptografar</DropdownMenu.DropdownMenuRadioItem>
+        <DropdownMenu.DropdownMenuRadioItem value ='decrypt'>Descriptografar</DropdownMenu.DropdownMenuRadioItem>
+        </DropdownMenu.DropdownMenuRadioGroup>
+    </DropdownMenu.DropdownMenuContent>
   </DropdownMenu.Root>
 );
