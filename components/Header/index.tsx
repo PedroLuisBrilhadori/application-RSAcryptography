49ab50228c46b6d756e3160ff7fc2e8dfc14bc35
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-18 09:27:28 -0300

    style: change color and background

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -11,19 +11,19 @@ import {
 
 export default function Header() {
   return (
-    <header className="bg-white text-black">
-      <Popover className="relative bg-white">
+    <header className="bg-stone-800 text-white">
+      <Popover className="relative bg-stone-800">
         <div className="mx-auto max-w-7xl px-4 sm:px-6">
-          <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex items-center justify-between border-gray-100 py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <a href="#">
                 <LockClosedIcon
-                  className="h-8 w-auto sm:h-10 text-black"
+                  className="h-8 w-auto sm:h-10 text-white"
                 />
               </a>
             </div>
             <div className="-my-2 -mr-2 md:hidden">
-              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset">
+              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-stone-800 p-2 text-gray-400 hover:bg-gray-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset">
                 <span className="sr-only">Open menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </Popover.Button>
@@ -52,8 +52,8 @@ export default function Header() {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
-            <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform transition md:hidden">
+            <div className="divide-gray-50 rounded-lg bg-stone-800 shadow-lg ring-1 ring-black ring-opacity-5">
               <div className="px-5 pt-5 pb-6">
                 <div className="flex items-center justify-between">
                   <div>
@@ -62,7 +62,7 @@ export default function Header() {
                     />
                   </div>
                   <div className="-mr-2">
-                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset">
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-stone-800 p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset">
                       <span className="sr-only">Close menu</span>
                       <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                     </Popover.Button>
@@ -72,18 +72,18 @@ export default function Header() {
               <div className="space-y-6 py-6 px-5">
                 <div className="grid grid-cols-1 gap-y-4 gap-x-8">
                   <a href='/' className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50">
-                    <HomeIcon className="h-6 w-6 flex-shrink-0 text-indigo-600 text-black" aria-hidden="true" />
-                    <span className="ml-3 text-base font-medium text-gray-900">Inicio</span>
+                    <HomeIcon className="h-6 w-6 flex-shrink-0" aria-hidden="true" />
+                    <span className="ml-3 text-base font-medium">Inicio</span>
                   </a>
 
                   <a href='/' className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50">
-                    <LockClosedIcon className="h-6 w-6 flex-shrink-0 text-indigo-600 text-black" aria-hidden="true" />
-                    <span className="ml-3 text-base font-medium text-gray-900">Criptografar</span>
+                    <LockClosedIcon className="h-6 w-6 flex-shrink-0" aria-hidden="true" />
+                    <span className="ml-3 text-base font-medium">Criptografar</span>
                   </a>
 
                   <a href='/' className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50">
-                    <LockOpenIcon className="h-6 w-6 flex-shrink-0 text-indigo-600 text-black" aria-hidden="true" />
-                    <span className="ml-3 text-base font-medium text-gray-900">Descriptografar</span>
+                    <LockOpenIcon className="h-6 w-6 flex-shrink-0" aria-hidden="true" />
+                    <span className="ml-3 text-base font-medium">Descriptografar</span>
                   </a>
                 </div>
               </div>
