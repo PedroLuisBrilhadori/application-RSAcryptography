9dac28f3f486a15891b650738961a05d73788aee
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-19 11:22:02 -0300

    feat: add component ResultText

--- pages/encrypt.tsx
+++ pages/encrypt.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
+import ResultText from '../components/ResultText'
 import TextField from '../components/TextField'
 
 export default function Encrypt() {
+
   return (
     <>
       <Header />
       <section>
-        <TextField label='Informe seu texto aqui'/>
+        <TextField label='Informe seu texto aqui' />
+      
+        <ResultText result='' id='textResult' />
       </section>
     </>
   )
