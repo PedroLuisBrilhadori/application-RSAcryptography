3382eadfb0619555dd292c5096fa6c0ee005c1db
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-18 22:01:01 -0300

    fix: correct tag

--- components/ResultText/index.tsx
+++ components/ResultText/index.tsx
@@ -9,7 +9,7 @@ const ResultText: NextPage<Props> = (props) => {
     const {result} = props;
     return (
         <div className='m-10 w-auto h-max'>
-            <span className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]'>{result}</span>
+            <p className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]'>{result}</p>
         </div>
     )
 }
