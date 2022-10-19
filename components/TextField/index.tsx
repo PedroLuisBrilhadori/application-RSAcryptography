3c2f7d55a3dcfdd0d43a5771aea31a49b9acd72a
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-19 11:18:55 -0300

    refactor: remove props not necessary in this moment

--- components/TextField/index.tsx
+++ components/TextField/index.tsx
@@ -1,19 +1,17 @@
 import { NextPage } from 'next';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
     label: string;
     id: string;
-    textInput: string;
-    changeState: React.FormEvent<HTMLInputElement> | undefined | void;
 }
 
 const TextField: NextPage<Props> = (props) => {
-    const { label, id, textInput, changeState } = props;
+    const { label, id } = props;
 
     return (
         <div className='m-10 w-auto h-max'>
-            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id} onChange={() => changeState}>{textInput}</input>
+            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id}  />
         </div>
     )
 }
