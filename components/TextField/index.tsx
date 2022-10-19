3007d6413e8a53c3111f051d4cba2acfe49b3f6e
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-18 22:05:26 -0300

    feat: add id in the input

--- components/TextField/index.tsx
+++ components/TextField/index.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 interface Props {
     label: string;
+    id: string;
 }
 
 const TextField: NextPage<Props> = (props) => {
-    const { label } = props;
+    const { label, id } = props;
 
     return (
         <div className='m-10 w-auto h-max'>
-            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' />
+            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id}/>
         </div>
     )
 }
