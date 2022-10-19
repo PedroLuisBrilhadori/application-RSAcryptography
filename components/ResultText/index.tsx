1e6dda2f6fd3daa7fe78a0e2b7394474db262839
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-18 21:56:19 -0300

    feat: add new component (resultText)

---
+++ components/ResultText/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next';
+import React from 'react'
+
+interface Props {
+    result: string;
+}
+
+const ResultText: NextPage<Props> = (props) => {
+    const {result} = props;
+    return (
+        <div className='m-10 w-auto h-max'>
+            <span className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]'>{result}</span>
+        </div>
+    )
+}
+
+export default ResultText
