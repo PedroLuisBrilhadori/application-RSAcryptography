4d4b8e9c6c0389d6fa41891162b512e596c4d4a8
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-18 22:25:26 -0300

    feat: add event onchange

--- components/TextField/index.tsx
+++ components/TextField/index.tsx
@@ -4,14 +4,15 @@ import React from 'react'
 interface Props {
     label: string;
     id: string;
+    changeState: React.FormEvent<HTMLInputElement> | undefined;
 }
 
 const TextField: NextPage<Props> = (props) => {
-    const { label, id } = props;
+    const { label, id, changeState } = props;
 
     return (
         <div className='m-10 w-auto h-max'>
-            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id}/>
+            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id} onChange={() => changeState} />
         </div>
     )
 }
