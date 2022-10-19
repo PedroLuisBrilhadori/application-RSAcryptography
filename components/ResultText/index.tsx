96623af375247634601f0db9e9c0291292b52bbd
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-19 11:21:00 -0300

    feat: add id in the ResultText

--- components/ResultText/index.tsx
+++ components/ResultText/index.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 
 interface Props {
     result: string;
+    id: string;
 }
 
 const ResultText: NextPage<Props> = (props) => {
-    const {result} = props;
+    const {result, id} = props;
     return (
         <div className='m-10 w-auto h-max'>
-            <p className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]'>{result}</p>
+            <p className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id}>{result}</p>
         </div>
     )
 }
