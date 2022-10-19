d28cb83e42b96e8be8d1f9ef1b3478bdccabb54f
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-18 21:25:24 -0300

    style: add style to the component textfield

--- components/TexField/index.tsx
+++ components/TexField/index.tsx
@@ -6,10 +6,13 @@ interface Props {
 }
 
 const TextField: NextPage<Props> = (props) => {
-const { label } = props;
-  return (
-    <input type='text' placeholder={label} />
-  )
+    const { label } = props;
+
+    return (
+        <div className='m-10 w-auto h-max'>
+            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' />
+        </div>
+    )
 }
 
 export default TextField;
