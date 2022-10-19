49ac4e87bdec1a44ad84322d7aeba68ca1472c9f
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-18 22:39:09 -0300

    feat: add textInput in the component

--- components/TextField/index.tsx
+++ components/TextField/index.tsx
@@ -4,15 +4,16 @@ import React from 'react'
 interface Props {
     label: string;
     id: string;
-    changeState: void;
+    textInput: string;
+    changeState: React.FormEvent<HTMLInputElement> | undefined | void;
 }
 
 const TextField: NextPage<Props> = (props) => {
-    const { label, id, changeState } = props;
+    const { label, id, textInput, changeState } = props;
 
     return (
         <div className='m-10 w-auto h-max'>
-            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id} onChange={() => changeState} />
+            <input type='text' placeholder={label} className='w-full h-60 rounded-xl bg-white border-2 border-stone-800 text-black pb-[200px] pl-[10px]' id={id} onChange={() => changeState}>{textInput}</input>
         </div>
     )
 }
