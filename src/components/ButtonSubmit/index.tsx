2ef7c617bc89e6616c76c89cb432ad6313c468df
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-21 23:24:43 -0300

    feat: add new component (button of the type submit)

---
+++ src/components/ButtonSubmit/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ButtonSubmit() {
+  return (
+    <div className='items-center w-full text-center'>
+        <button type='submit' className='self-center text-center bg-stone-800 w-11/12 rounded-xl text-white text-lg p-[5px] border-black border-2'>Criptografar</button>
+    </div>
+  )
+}
