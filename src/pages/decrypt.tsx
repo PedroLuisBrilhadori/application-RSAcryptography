9e5b6937c89ffea70cf01858a85e5d8b214db691
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-21 23:40:54 -0300

    feat: add form and add textField on each page

--- src/pages/decrypt.tsx
+++ src/pages/decrypt.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ButtonSubmit from '../components/ButtonSubmit';
 import Header from '../components/Header'
 import ResultText from '../components/ResultText'
 import TextField from '../components/TextField'
 
 export default function Decrypt() {
+  const [changeText, setChangeText] = useState('');
+
+  function handleOnChange(e: any){    
+    setChangeText(e.target.value);
+  }
+
   return (
     <>
       <Header />
-      <section>
-        <TextField label='Informe seu texto aqui' id='textDecrypt' />
-        
+      
+      <form action='/api/' method='post'>
+        <div className='m-10 w-auto h-max'>
+          <textarea placeholder='Informe seu texto aqui' className='w-full h-[150px] rounded-xl bg-white border-2 border-stone-800 text-black pl-[10px] overflow-auto' id='textDecrypt' onChange={handleOnChange}>
+            {changeText}
+          </textarea>
+        </div>
+      
         <ResultText result='Resultado:' id='textResult' />
-      </section>
+
+        <ButtonSubmit text='Descriptografar' />
+      </form>
     </>
   )
 }
