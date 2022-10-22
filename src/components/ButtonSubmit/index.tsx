069d63110020f6c393106f550bacbfd90b4f06a3
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-21 23:39:18 -0300

    feat: add props text

--- src/components/ButtonSubmit/index.tsx
+++ src/components/ButtonSubmit/index.tsx
@@ -1,9 +1,18 @@
+import { NextPage } from 'next';
 import React from 'react'
 
-export default function ButtonSubmit() {
-  return (
-    <div className='items-center w-full text-center'>
-        <button type='submit' className='self-center text-center bg-stone-800 w-11/12 rounded-xl text-white text-lg p-[5px] border-black border-2'>Criptografar</button>
-    </div>
-  )
+interface Props {
+    text: String;
 }
+
+const ButtonSubmit: NextPage<Props> = (props) => {
+    const {text} = props;
+
+    return (
+        <div className='items-center w-full text-center'>
+            <button type='submit' className='self-center text-center bg-stone-800 w-11/12 rounded-xl text-white text-lg p-[5px] border-black border-2'>{text}</button>
+        </div>
+    )
+}
+
+export default ButtonSubmit;
